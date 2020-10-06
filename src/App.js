de364a9cc2c1b681768545135eb6d67bf0f96218
YuraLviv <ivanivyura@gmail.com>
2020-10-06 22:57:41 +0300

    added basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-   
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src="https://img1.freepng.ru/20171220/gze/twitter-logo-png-5a3a1851372e76.0876249315137567532269680.jpg" alt="!!" />
+      </header>
+      <nav className='nav'>
+        <div>
+          <a href='#'>Profile</a>
+        </div>
+        <div>
+          <a href='#'>Messages</a>
+        </div>
+        <div>
+          <a href='#'>News</a>
+        </div>
+        <div>
+          <a href='#'>Music</a>
+        </div>
+        <div>
+          <a href='#'>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <img src='https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg'/>
+      </div>
+    </div>
   );
 }
 
