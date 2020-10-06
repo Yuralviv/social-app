7ec89f0e8950f9600e6c12ba995a680121d551ce
YuraLviv <ivanivyura@gmail.com>
2020-10-07 00:14:13 +0300

    added structure for application

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg" />
+      </div>
+      <div>ava + decs</div>
+      <div>new post</div>
+      <div>post1</div>
+      <div>post 2</div>
+    </div>
+  );
+};
+
+export default Profile;
