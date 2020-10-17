d3ec1e5b8a59fe143f4d095ac5d50ae1811262c8
YuraLviv <ivanivyura@gmail.com>
2020-10-17 16:06:46 +0300

    add redux settings for application and update Dialogs component

--- src/components/dialogs/Dialogs.js
+++ src/components/dialogs/Dialogs.js
@@ -2,23 +2,29 @@ import React from "react";
 import DialogsItem from "./dialogsItem/DialogsItem";
 import classes from "./Dialogs.module.css";
 import DialogsMessage from "./dialogsMessage/DialogsMessage";
+import { useSelector, shallowEqual } from "react-redux";
 
-const Dialogs = (props) => {
+const Dialogs = ({ id, message, name }) => {
+  const dialogsList = useSelector(
+    (state) => state.dialogsReducer.dialogs,
+    shallowEqual
+  );
+  const messageList = useSelector(
+    (state) => state.dialogsReducer.messages,
+    shallowEqual
+  );
+
+  const dialogsElements = dialogsList.map((d) => (
+    <DialogsItem name={d.name} key={d.id} id={d.id} />
+  ));
+  const messagesElements = messageList.map((m) => (
+    <DialogsMessage message={m.message} key={m.id} />
+  ));
+
   return (
     <div className={classes.dialogs}>
-      <div className={classes.dialogsItems}>
-        <DialogsItem name="Yura" id="1" />
-        <DialogsItem name="Anna" id="2" />
-        <DialogsItem name="Sophia" id="3" />
-        <DialogsItem name="Orest" id="4" />
-        <DialogsItem name="Alla" id="5" />
-      </div>
-      <div className={classes.messages}>
-        <DialogsMessage message="hello" />
-        <DialogsMessage message="hello" />
-        <DialogsMessage message="hello" />
-        <DialogsMessage message="hello" />
-      </div>
+      <div className={classes.dialogsItems}>{dialogsElements}</div>
+      <div className={classes.messages}>{messagesElements}</div>
     </div>
   );
 };
