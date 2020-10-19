15b398845da5806557fc8d8f98e74e8338716307
YuraLviv <ivanivyura@gmail.com>
2020-10-19 23:14:02 +0300

    added axios and added users from api

--- src/redux/reducer/userReducer.js
+++ src/redux/reducer/userReducer.js
@@ -2,34 +2,34 @@ import * as types from "../action/ActionTypes";
 
 const initialState = {
   users: [
-    {
-      id: 1,
-      followed: true,
-      fullname: "Roma",
-      status: "i`m boss",
-      location: { country: "UA", city: "lviv" },
-    },
-    {
-      id: 2,
-      followed: false,
-      fullname: "Yura",
-      status: "fullstack",
-      location: { country: "UA", city: "lviv" },
-    },
-    {
-      id: 3,
-      followed: true,
-      fullname: "Petro",
-      status: "pyrhon dev",
-      location: { country: "UA", city: "lviv" },
-    },
-    {
-      id: 4,
-      followed: true,
-      fullname: "Dimych",
-      status: "_zar_",
-      location: { country: "UA", city: "lviv" },
-    },
+    // {
+    //   id: 1,
+    //   followed: true,
+    //   fullname: "Roma",
+    //   status: "i`m boss",
+    //   location: { country: "UA", city: "lviv" },
+    // },
+    // {
+    //   id: 2,
+    //   followed: false,
+    //   fullname: "Yura",
+    //   status: "fullstack",
+    //   location: { country: "UA", city: "lviv" },
+    // },
+    // {
+    //   id: 3,
+    //   followed: true,
+    //   fullname: "Petro",
+    //   status: "pyrhon dev",
+    //   location: { country: "UA", city: "lviv" },
+    // },
+    // {
+    //   id: 4,
+    //   followed: true,
+    //   fullname: "Dimych",
+    //   status: "_zar_",
+    //   location: { country: "UA", city: "lviv" },
+    // },
   ],
 };
 
