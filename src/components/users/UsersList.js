4646f1f1948d67186ca5cc8cb650344a384cd844
YuraLviv <ivanivyura@gmail.com>
2020-10-23 23:07:14 +0300

    added pagination for users components

--- src/components/users/UsersList.js
+++ src/components/users/UsersList.js
@@ -1,33 +1,83 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import * as axios from "axios";
 import UserItem from "./UserItem";
-import { setUsers } from "../../redux/action/UserAction";
+import {
+  setUsers,
+  setCurrentPage,
+  setTotalUserCount,
+} from "../../redux/action/UserAction";
+import classes from "./Users.module.css";
 
-const UsersList = (props) => {
-  const dispatch = useDispatch();
-
+const UsersList = () => {
   const userList = useSelector(
     (state) => state.userReducer.users,
+  );
+
+  const pageSize = useSelector(
+    (state) => state.userReducer.pageSize,
     shallowEqual
   );
 
-    if(userList.length === 0) {
-      axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response) => {
-        dispatch(setUsers(response.data.items))
-      })
-    }
+  const totalUsersCount = useSelector(
+    (state) => state.userReducer.totalUsersCount,
+    shallowEqual
+  );
+
+  const currentPage = useSelector(
+    (state) => state.userReducer.currentPage,
+    shallowEqual
+  );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`
+      )
+      .then((response) => {
+        dispatch(setUsers(response.data.items));
+        dispatch(setTotalUserCount(response.data.totalCount));
+      });
+  }, []);
+
+  const onPageChanged = (pageNumber) => {
+    dispatch(setCurrentPage(pageNumber));
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${pageSize}`
+      )
+      .then((response) => {
+        dispatch(setUsers(response.data.items));
+      });
+  };
+
+  const pagesCount = Math.ceil(totalUsersCount / pageSize);
+  const pages = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
+  }
 
   const userElements = userList.map((u) => (
-    <UserItem
-      key={u.id}
-      user={u}
-      followed={u.followed}
-      fullname={u.name}
-    />
+    <UserItem key={u.id} user={u} followed={u.followed} fullname={u.name} />
   ));
+
   return (
     <div>
+      <div>
+        {pages.map((p) => (
+          <span
+            key={p}
+            className={currentPage === p && classes.selectedPage}
+            onClick={() => {
+              onPageChanged(p);
+            }}
+          >
+            {` ${p}  `}
+          </span>
+        ))}
+      </div>
       <div>{userElements}</div>
     </div>
   );
--- src/redux/action/ActionTypes.js
+++ src/redux/action/ActionTypes.js
@@ -5,7 +5,6 @@ export const SET_USER_PROFILE = 'SET_USER_PROFILE';
 export const DELETE_POST = 'DELETE_POST';
 export const SAVE_PHOTO_SUCCESS = 'SAVE_PHOTO_SUCCESS';
 
-
 // dialogs
 export const SEND_MESSAGE = 'SEND_MESSAGE';
 
@@ -14,3 +13,6 @@ export const USER_FOLLOW = 'USER_FOLLOW';
 export const USER_UNFOLLOW = 'USER_UNFOLLOW';
 export const SET_USERS = 'SET_USERS';
 
+// currentPage
+export const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+export const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
